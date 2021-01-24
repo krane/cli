6ff77e2b05722fc9d868d1a1e62b895346b3264c
David Castaneda <dcast188@fiu.edu>
2021-01-23 19:29:26 -0500

    (feat) sessions command

---
+++ src/commands/sessions.ts
@@ -0,0 +1,142 @@
+import cli from "cli-ux";
+
+import BaseCommand from "../base";
+import { Session } from "@krane/common";
+
+export default class Sessions extends BaseCommand {
+  static description = "Create, list or remove user sessions";
+
+  static examples = [
+    "$ krane sessions ls",
+    "$ krane sessions list",
+    "$ krane secrets create",
+    "$ krane secrets rm my-user",
+    "$ krane secrets remove my-user",
+  ];
+
+  static args = [
+    {
+      name: "subcommand",
+      required: true,
+      options: ["ls", "list", "create", "rm", "remove"],
+      description: "[list, create, remove]",
+    },
+    {
+      name: "user",
+      required: false,
+      description: "name of the session user ",
+    },
+  ];
+
+  async run() {
+    const { args } = this.parse(Sessions);
+
+    switch (args.subcommand) {
+      case "create":
+        if (!args.user) {
+          args.user = await cli.prompt(
+            "What should we name this session? For example `bob` or `my-deployment-ci`",
+            { required: true }
+          );
+        }
+
+        await this.create(args.user);
+        break;
+      case "rm":
+      case "remove":
+        if (!args.user) {
+          args.user = await cli.prompt("Name of the user to create?", {
+            required: true,
+          });
+        }
+        await this.remove(args.user);
+        break;
+      case "ls":
+      case "list":
+        if (args.user) {
+          await this.listOne(args.user);
+          return;
+        }
+        await this.list();
+        break;
+      default:
+        this.error(`Unknown command ${args.subcommand}`);
+    }
+  }
+
+  async create(user: string) {
+    const client = await this.getKraneClient();
+
+    try {
+      await client.createSession(user);
+    } catch (e) {
+      this.error(e?.response?.data ?? "Unable to create session");
+    }
+
+    this.log(`Succesfully created session for ${user}\n`);
+    this.log(`  Run the below command to list details about ${user}'s session`);
+    this.log(`  $ krane sessions list ${user}\n`);
+  }
+
+  async remove(user: string) {
+    const client = await this.getKraneClient();
+
+    const sessions = await client.getSessions();
+    const sessionToRemove = sessions.find((s) => user == s.user);
+    if (!sessionToRemove) {
+      this.error(`Session ${user} not found`);
+    }
+
+    try {
+      await client.deleteSession(sessionToRemove.id);
+      this.log(`Succesfully deleted session for ${user}`);
+    } catch (e) {
+      this.error(e?.response?.data ?? "Unable to remove session");
+    }
+  }
+
+  async listOne(user: string) {
+    const client = await this.getKraneClient();
+
+    let sessions: Session[];
+    try {
+      sessions = await client.getSessions();
+    } catch (e) {
+      this.error(e?.response?.data ?? "Unable to list sessions");
+    }
+
+    cli.table(
+      sessions.filter((s) => user == s.user),
+      {
+        expires: {
+          get: (session) => session.expires_at,
+        },
+        token: {
+          get: (session) => session.token,
+        },
+      }
+    );
+  }
+
+  async list() {
+    const client = await this.getKraneClient();
+
+    let sessions: Session[];
+    try {
+      sessions = await client.getSessions();
+    } catch (e) {
+      this.error(e?.response?.data ?? "Unable to list sessions");
+    }
+
+    cli.table(sessions, {
+      user: {
+        get: (session) => session.user,
+        minWidth: 10,
+      },
+      expires: {
+        get: (session) => session.expires_at,
+        minWidth: 15,
+      },
+    });
+  }
+}
